b989a635863a61aaef3f4158fa0aa2540e002e6b
Jeremy Chon <jaedee95@gmail.com>
2019-07-30 14:59:32 -0500

    able to create/register a traveler

--- controllers/travelController.js
+++ controllers/travelController.js
@@ -1,22 +1,51 @@
 const express = require('express')
 const router = express()
+const Traveler = require('../models/traveler')
 
 
-// LOGIN/REGISTER ROUTE
+// LOGIN ROUTE
 router.get('/login', async (req, res, next) => {
-	res.render('traveler/login')
+	res.render('traveler/login.ejs')
 })
 
-// // INDEX ROUTE
-// router.get('/', async (req, res, next) => {
+router.post('/login', async (req, res, next) => {
+	
+})
 
-// 	try {
-// 		res.render('traveler/home')
+// REGISTER ROUTE
+router.get('/register', async (req, res, next) => {
+	res.render('traveler/register.ejs')
+})
 
-// 	} catch (err) {
-// 		next(err)
-// 	}
-// })
+router.post('/register', async (req, res, next) => {
+	
+	try {
+		
+		const createdTraveler = await Traveler.create({
+			email: req.body.email,
+			password: req.body.password
+		})
+
+		console.log(createdTraveler);
+
+		res.redirect('/traveler/home.ejs')
+
+	} catch (err) {
+		next(err)
+	}
+
+})
+
+// INDEX ROUTE
+router.get('/', async (req, res, next) => {
+
+	try {
+		res.render('traveler/home.ejs')
+
+	} catch (err) {
+		next(err)
+	}
+})
 
 
 
