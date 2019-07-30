1130247029d2e8c4c9ff3c5294c4739ef587a825
jordan woods <jordanwoods@jordans-MacBook-Pro.local>
2019-07-30 15:07:15 -0500

    Investor overview page now rendering after new account creation

--- controllers/investController.js
+++ controllers/investController.js
@@ -6,14 +6,19 @@ router.get('/new', (req, res) => {
   res.render('investor/new.ejs');
 });
 
+router.get('/show', (req, res) => {
+  res.render('investor/show.ejs');
+});
 
 
 router.post('/new', async (req, res, next) => {
     try {
-      const createdInvestor = Investor.create({
+      const createdInvestor = await Investor.create({
         email: req.body.email,
         password: req.body.password
       });
+
+      res.redirect('/investor/show');
     } catch (err) {
       next(err)
     }
