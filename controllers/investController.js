60f337309af38525eb8643f2cee631e9fae30143
jordan woods <jordanwoods@jordans-MacBook-Pro.local>
2019-07-30 14:52:42 -0500

    Added functionality for creating an investor account

--- controllers/investController.js
+++ controllers/investController.js
@@ -1,5 +1,23 @@
 const express = require('express')
 const router = express()
+const Investor = require('../models/investor')
+
+router.get('/new', (req, res) => {
+  res.render('investor/new.ejs');
+});
+
+
+
+router.post('/new', async (req, res, next) => {
+    try {
+      const createdInvestor = Investor.create({
+        email: req.body.email,
+        password: req.body.password
+      });
+    } catch (err) {
+      next(err)
+    }
+});
 
 
 
