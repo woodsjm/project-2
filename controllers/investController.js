4a601c91bb1c8fcd0bdae49595e0ebe0b264b04c
Jeremy Chon <jaedee95@gmail.com>
2019-08-01 07:50:24 -0500

    investor has cookie and users can't get into investor sites without being an investor

--- controllers/investController.js
+++ controllers/investController.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const bcrypt = require('bcryptjs')
 const Investor = require('../models/investor')
 const Bond = require('../models/bonds')
 const Policy = require('../models/flightPolicy')
@@ -16,30 +17,72 @@ router.get('/login', (req, res) => {
 
 //Investor Login Route
 router.post('/login', async (req, res, next) => {
-    try {
-      const logInvestor = await Investor.findOne({email: req.body.email})
 
-      if (!logInvestor) {
-        console.log("this account doesn't exit")
-        res.redirect('/investor/login')
-      } else {
+  try {
+
+    const logInvestor = await Investor.findOne({email: req.body.email})
+
+    if (logInvestor) {
+
+      if (bcrypt.compareSync(req.body.password, logInvestor.password) === true) {
+
+        req.session.investorId = logInvestor._id
+        req.session.email = logInvestor.email
+        req.session.loggedIn = true
+        req.session.investor = true
+
+        console.log(req.session);
+
         res.redirect('/investor/show/' + logInvestor.id)
+
+      } else {
+        res.redirect('/investor/login')
       }
-    } catch (err) {
-      next(err)
+
+    } else {
+      res.redirect('/investor/login')
     }
+
+  } catch (err) {
+    next(err)
+  }
 });
+
+// Logout Route
+router.get('/logout', (req, res, next) => {
+  try {
+
+    req.session.destroy()
+    console.log(req.session);
+
+    console.log('Come again');
+
+    res.redirect('/investor/login')
+
+  } catch (err) {
+    next(err)
+  }
+})
 
 
 // Create Investor Route
 router.post('/new', async (req, res, next) => {
+    const password = req.body.password
+
+    const hashedPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10))
+
+    req.body.password = hashedPassword
+
     try {
-      const createdInvestor = await Investor.create({
-        email: req.body.email,
-        password: req.body.password
-      });
+      const createdInvestor = await Investor.create(req.body)
+
+      req.session.investorId = createdInvestor._id
+      req.session.email = createdInvestor.email
+      req.session.loggedIn = true
+      req.session.investor = true
 
       res.redirect('/investor/show/' + createdInvestor.id);
+
     } catch (err) {
       next(err)
     }
@@ -48,11 +91,13 @@ router.post('/new', async (req, res, next) => {
 // Edit Investor Route
 router.get('/:id/edit', async (req, res, next) => {
   try {
-    const foundInvestor = await Investor.findById(req.params.id);
+    if (req.session.loggedIn == true && req.session.investor == true) {
+      const foundInvestor = await Investor.findById(req.params.id);
 
-    res.render('investor/edit.ejs', {
-      investor: foundInvestor
-    });
+      res.render('investor/edit.ejs', {
+        investor: foundInvestor
+      });
+    }
   } catch (err) {
     next(err)
   }
@@ -72,6 +117,7 @@ router.delete('/:id', async (req, res, next) => {
   }
 });
 
+// Update Investor Account Route
 router.put('/:id', async (req, res, next) => {
   try {
     
@@ -84,21 +130,21 @@ router.put('/:id', async (req, res, next) => {
   }
 });
 
-//  
 
 //Show Investor Route
 router.get('/show/:id', async (req, res, next) => {
   try {
-    const foundInvestor = await Investor.findById(req.params.id);
-    const currentOffering = await Bond.find({});
 
+    if (req.session.loggedIn == true && req.session.investor == true) {
+      const foundInvestor = await Investor.findById(req.params.id);
+      const currentOffering = await Bond.find({});
 
-
-    console.log(foundInvestor);
-    res.render('investor/show.ejs', {
-      investor: foundInvestor,
-      bonds: currentOffering
-    });
+      console.log(foundInvestor);
+      res.render('investor/show.ejs', {
+        investor: foundInvestor,
+        bonds: currentOffering
+      });
+    }
   } catch (err) {
     next(err)
   }
